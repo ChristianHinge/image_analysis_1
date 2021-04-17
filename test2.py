5768ced7759444cb19d95b5d03522ee0ec4d2da0
Christian Hinge <christian@hingepedersen.dk>
2021-04-17 15:21:19 +0200

    trainloader

--- test2.py
+++ test2.py
@@ -1,9 +1,12 @@
 
 #%%
 import numpy as np
+import cv2
 from PIL import Image
 from matplotlib import pyplot as plt
-import os 
+import os
+import scipy
+from scipy import ndimage as nd
 
 data_dir = "data/Patients_CT"
 
@@ -57,51 +60,101 @@ plt.figure()
 plt.imshow(Y[0,:,:])
 
 #%% Augmentation
-# define: patient, slice
 
-angles = [-30, 30]
 
-def AUG(X,Y,angles,scale)
+# function cv2_clipped_zoom taken from:
+# https://stackoverflow.com/questions/37119071/scipy-rotate-and-zoom-an-image-without-changing-its-dimensions/37121993#37121993
+# OBS: function has been modified to have different zoom on x and y axes
+def cv2_clipped_scale(img, scale_x, scale_y):
+    """
+    Center scale in/out of the given image and returning an enlarged/shrinked view of 
+    the image without changing dimensions
+    INPUT
+        img : Image array
+        scale_x : amount of scale on x axis as a ratio (0 to Inf)
+        scale_y : amount of scale on y axis as a ratio (0 to Inf)
+    """
+    height, width = img.shape[:2] # It's also the final desired shape
+    new_height, new_width = int(height * scale_y), int(width * scale_x)
+
+    ### Crop only the part that will remain in the result (more efficient)
+    # Centered bbox of the final desired size in resized (larger/smaller) image coordinates
+    y1, x1 = max(0, new_height - height) // 2, max(0, new_width - width) // 2
+    y2, x2 = y1 + height, x1 + width
+    bbox = np.array([y1,x1,y2,x2])
+    # Map back to original image coordinates
+    bbox = (bbox / [scale_y,scale_x,scale_y,scale_x]).astype(np.int)
+    y1, x1, y2, x2 = bbox
+    cropped_img = img[y1:y2, x1:x2]
+
+    # Handle padding when downscaling
+    resize_height, resize_width = min(new_height, height), min(new_width, width)
+    pad_height1, pad_width1 = (height - resize_height) // 2, (width - resize_width) //2
+    pad_height2, pad_width2 = (height - resize_height) - pad_height1, (width - resize_width) - pad_width1
+    pad_spec = [(pad_height1, pad_height2), (pad_width1, pad_width2)] + [(0,0)] * (img.ndim - 2)
+
+    result = cv2.resize(cropped_img, (resize_width, resize_height))
+    result = np.pad(result, pad_spec, mode='constant')
+    assert result.shape[0] == height and result.shape[1] == width
+    return result
+
+
+def AUG(X,Y,angles,scales):
     # function can perform horizontal flip, rotation, scaling and normalization
     #
     # INPUT
     # X : [xdim, ydim, 2] image values for brain and bone window
     # Y : [xdim, ydim] segmentation mask image values
     # angles : [angle_low, angle_high] defines range of angles to randomly choose from
-    # scale : [scale_low, scale_high] defines range of scaling to randomly choose from
+    # scales : [scale_low, scale_high] defines range of scaling to randomly choose from
     #
     # OUTPUT
     # X_AUG : [xdim, ydim, 2] image values for augmented brain and bone window
     # Y : [xdim, ydim] augmented segmentation mask image values
 
-    import scipy
-    from scipy import ndimage as nd
-
-    X_AUG = x
+    X_AUG = X
     Y_AUG = Y
 
     # flip (horizontal - xdim)
     flip = np.random.randint(2,size=1) # randomly select 0 or 1 (no flip or flip)
     if flip == 1:
-        X_FLIP = np.flip(X_AUG,axis=1)
-        Y_FLIP = np.flip(Y_AUG,axis=1)
+        X_AUG = np.flip(X_AUG,axis=2) # flips bone and brain image
+        Y_AUG = np.flip(Y_AUG,axis=2) # flips mask
 
     # rotate
+    # rotation in x,y-plane (axes 0,1)
     # randomize angle selection
     angle = np.random.uniform(low = angles[0], high = angles[1], size = [1,1])
-    X_AUG = nd.rotate(X_AUG,angle,axes = 0,1) # rotation in x,y-plane
-    Y_AUG = nd.rotate(Y_AUG,angle,axes = 0,1) # rotation in x,y-plane
+    rows,cols = Y_AUG.shape
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
+    Y_AUG = cv2.warpAffine(Y_AUG,M,(cols,rows)) # rotate mask
+    X_AUG[:,:,0] = cv2.warpAffine(X_AUG[:,:,0],M,(cols,rows)) # rotate bone
+    X_AUG[:,:,1] = cv2.warpAffine(X_AUG[:,:,1],M,(cols,rows)) # rotate brain
 
     # scale
-
-
-    # normalization
-    max_val = 255
-    X_AUG = X_AUG/max_val
-    Y_AUG = Y_AUG/max_val
-
+    scale = np.random.uniform(low = scales[0], high = scales[1], size = [1,2])
+    # cv2_clipped_scale(image, scale_x, scale_y)
+    X_AUG = cv2_clipped_scale(X_AUG, scale[0], scale[1])
+    Y_AUG = cv2_clipped_scale(Y_AUG, scale[0], scale[1])
+    
+    # elastic transform
+    # albumentations.augmentations.geometric.transforms.ElasticTransform (alpha=1, sigma=50, alpha_affine=50, interpolation=1, border_mode=4, value=None, mask_value=None, always_apply=False, approximate=False, p=0.5)
 
     return X_AUG, Y_AUG
+
+#%%
+# NORMALIZE
+
+
+# AUGMENTATION
+# define augmentation variables
+angles = [-30, 30]      # range of angles as [angle_low, angle_high]
+scales = [0.8, 1.2]     # range of scaling as [scale_low, scale_high]
+
+
+X_AUG, Y_AUG = AUG(X,Y,angles,scales)
+
+
 
 #%% U-net 
 
@@ -123,6 +176,7 @@ import os
 
 
 class Unet():
+    
     def __init__(self,batch_size,X_dim,Y_dim,n_channels):
         # Input shape
         self.batch_size = batch_size
