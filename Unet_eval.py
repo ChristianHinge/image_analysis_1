change normalization and normalization folders to be correct

Unet_eval.py
@@ -53,8 +53,8 @@ for i, ID in enumerate(eval_IDs):
     sl = ID.split("_")[3]
 
       # Load bone and brain slice
-    im_bone  = np.load(data_dir + f"/{pt}/brain/{sl}.npy")
-    im_brain = np.load(data_dir + f"/{pt}/bone/{sl}.npy")
+    im_bone  = np.load(data_dir + f"/{pt}/bone/{sl}.npy")
+    im_brain = np.load(data_dir + f"/{pt}/brain/{sl}.npy")
     im_seg = np.load(data_dir + f"/{pt}/seg/{sl}.npy")
     
     X_eval_imgs[i,:,:,0] = im_bone
@@ -84,15 +84,18 @@ for ii,ID in enumerate(eval_IDs):
 X_eval_imgs_hem = X_eval_imgs[eval_IDs_hem_bool,:,:,:]
 Y_eval_imgs_hem = Y_eval_imgs[eval_IDs_hem_bool,:,:]
 
-#%% 
+
+#%% load model and make prediction 
+ 
 BS = 2
 
-#load model and make prediction 
 Unet_model = load_model(model_path)
 
 #Y_eval_pred_imgs = np.round(Unet_model.predict(X_eval_imgs,batch_size=BS).squeeze())
 Y_eval_pred = np.round(Unet_model.predict(X_eval,batch_size=BS).squeeze())
 
+
+#plot some predictions from the whole evaluation set
 idx = 22
 
 plt.subplot(2,3,4)
@@ -103,7 +106,7 @@ plt.subplot(2,3,6)
 plt.imshow(Y_eval_pred[idx,:,:])
 
 
-#%% ONLY hemorrhage slices from validation
+#%% make predictions on ONLY hemorrhage slices from evaluation set
 
 BS = 2
 
@@ -111,7 +114,8 @@ Y_eval_pred_hem = np.round(Unet_model.predict(X_eval_imgs_hem,batch_size=BS).squ
 
 Y_eval_true_hem = Y_eval_imgs_hem
 
-#%%
+
+#plot some predictions from ONLY hemorrhage images in evaluation set
 idx = 7
 
 plt.figure(figsize=(10,6))
@@ -162,6 +166,7 @@ def IoU_coef(y_true, y_pred,smooth=1):
     return (intersection + smooth) / (union + smooth)
 
 
+#%% Compute metrics for whole evaluation set
 
 DSC = []
 IoU = []
@@ -189,7 +194,7 @@ print('Mean Dice: {0}'.format(np.array(DSC).mean()))
 print('Mean IoU: {0}'.format(np.array(IoU).mean()))
 
 
-#%%
+#%% Compute metrics for ONLY hemorrhage part of evaluation set
 
 DSC_hem = []
 IoU_hem = []
@@ -214,27 +219,6 @@ for b in range(int(Y_eval_true_hem.shape[0]/BS)):
 
 print('Mean Dice on hem: {0}'.format(np.array(DSC_hem).mean()))
 print('Mean IoU on hem: {0}'.format(np.array(IoU_hem).mean()))
-
-
-#%% testing if mean IOU from keras is the same 
-
-# a = Y_eval_true[20:40,:,:]
-# b = Y_eval_pred[20:40,:,:]
-
-# IOU_test = []
-
-# for ii in range(a.shape[0]):
-#     IoU_batch = IoU_coef(tf.convert_to_tensor(a[ii,:,:]), tf.convert_to_tensor(b[ii,:,:]))
-#     print(IoU_batch.numpy())
-#     IOU_test.append(IoU_batch.numpy())
-
-# print(np.array(IOU_test).mean())
-
-# m = tf.keras.metrics.MeanIoU(num_classes=2)
-# m.update_state(a, b)
-# c=m.result().numpy()
-# print(c)
-
 
 
 

dataloader_test.py
@@ -69,11 +69,11 @@ class DataLoader(Sequence):
             sl = ID.split("_")[3]
 
              # Load bone and brain slice
-            im_bone  = np.load(self.data_dir + f"/{pt}/brain/{sl}.npy")
-            im_brain = np.load(self.data_dir + f"/{pt}/bone/{sl}.npy")
+            im_bone  = np.load(self.data_dir + f"/{pt}/bone/{sl}.npy")
+            im_brain = np.load(self.data_dir + f"/{pt}/brain/{sl}.npy")
 
-            X[i,:,:,1] = im_bone
-            X[i,:,:,0] = im_brain
+            X[i,:,:,0] = im_bone
+            X[i,:,:,1] = im_brain
 
             # Load segmentation mask if training
             if load_Y:
@@ -170,8 +170,8 @@ class DataLoaderClassification(Sequence):
             sl = ID.split("_")[3]
 
              # Load bone and brain slice
-            im_bone  = np.load(self.data_dir + f"/{pt}/brain/{sl}.npy")
-            im_brain = np.load(self.data_dir + f"/{pt}/bone/{sl}.npy")
+            im_bone  = np.load(self.data_dir + f"/{pt}/bone/{sl}.npy")
+            im_brain = np.load(self.data_dir + f"/{pt}/brain/{sl}.npy")
             im_seg = np.load(self.data_dir + f"/{pt}/seg/{sl}.npy")
             
             X[i,:,:,0] = im_bone
@@ -205,10 +205,10 @@ def preprocess(pt,sl,data_dir = "data/normalized"):
     """
     preprocessing
     """
-
-    data_dir = "data/Patients_CT"
     out_dir = data_dir
-
+    
+    data_dir = "data/Patients_CT"
+    
     im_bone = np.array(Image.open(data_dir + f"/{pt}/bone/{sl}.jpg"))
     im_brain = np.array(Image.open(data_dir + f"/{pt}/brain/{sl}.jpg"))
     seg_path = data_dir + f"/{pt}/brain/{sl}_HGE_Seg.jpg"
@@ -245,15 +245,13 @@ def preprocess(pt,sl,data_dir = "data/normalized"):
 
 
 def normalize1(im):
-
+    
     im2 = (im-im.mean())/np.std(im)
     return im2
 
 def normalize2(im2):
-    #im2 = im2 > 100
-
-    im2 = im2 != 0
-
+    
+    im2 = im2 > 100
     return im2
 
 

train_unet.py
@@ -41,8 +41,7 @@ print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('
 
 data_dir = "data/Patients_CT"
 norm_dir = "/data/normalized"
-checkpoint_dir = "checkpoints/model_{epoch:02d}"
-checkpoint_dir2 = "checkpoints/model_{epoch:02d}.hdf5"
+checkpoint_dir = "checkpoints/model_{epoch:02d}.hdf5"
 
 
 cwd = os.getcwd()
@@ -56,13 +55,6 @@ if not os.path.exists(cwd + norm_dir):
         sl = ID.split("_")[3]
         preprocess(pt,sl)
 
-#%%
-
-for ID in IDs:
-    print(ID)
-    pt = ID.split("_")[1]
-    sl = ID.split("_")[3]
-    preprocess(pt,sl)
 
 #%%
 
@@ -135,7 +127,6 @@ def log_image(epoch, logs):
     plt.title("True")
     plt.tight_layout()
     
-    
     wandb.log({"test:" : plt}, step=epoch)
 
 
@@ -157,20 +148,16 @@ image_callback = keras.callbacks.LambdaCallback(on_epoch_end=log_image)
 csv_logger = CSVLogger(cwd + '/training.log')
 
 
-#Create a callback that saves the model's weights every 10 epochs
+#Create a callback that saves the model every 10 epochs
 BS = 2
 STEPS_PER_EPOCH = np.floor (len(train_IDs) / BS)
-SAVE_MODEL_EPOCHS = 2
+SAVE_MODEL_EPOCHS = 10
 DECAY_LR_EPOCHS = 80
 decay_steps = int(DECAY_LR_EPOCHS * STEPS_PER_EPOCH)
 
 
 cp_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_dir,
-    save_freq= int(SAVE_MODEL_EPOCHS * STEPS_PER_EPOCH))
-
-cp_callback2 = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_dir2,
     save_freq= int(SAVE_MODEL_EPOCHS * STEPS_PER_EPOCH))
 
 
@@ -181,7 +168,7 @@ Unet_model = unet.Unet_model
 
 
 #train model
-Unet_model.fit(d_train, batch_size = BS, epochs=120, callbacks = [image_callback, WandbCallback(), csv_logger, cp_callback, cp_callback2, LR_callback], validation_data=(X_val,Y_val), validation_batch_size = BS)
+Unet_model.fit(d_train, batch_size = BS, epochs=120, callbacks = [image_callback, WandbCallback(), csv_logger, cp_callback, LR_callback], validation_data=(X_val,Y_val), validation_batch_size = BS)
 
 Unet_model.save('Final_model')
 